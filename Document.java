596815e56a38156a3cdaa20aa52a1a26a2d807eb
0xR <114037840+0xRum@users.noreply.github.com>
2024-10-30 21:25:46 -0400

    Update Document.java

--- Document.java
+++ Document.java
@@ -1,15 +1,19 @@
-public class Document {
+import java.util.Date;
 
+public class Document {
+    // Attributes
     private String filename;
     private String filetype;
     private double fileSize;
     private Date uploadTime;
 
-    //Constructors
+    // Constructors
     public Document(String filename, String filetype, double fileSize, Date uploadTime) {
         this.filename = filename;
         this.filetype = filetype;
         this.fileSize = fileSize;
         this.uploadTime = uploadTime;
     }
+
+    //TODO: Implement Getters and Setters
 }
