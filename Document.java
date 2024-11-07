2425ee52702e0c4cb878d88e0ac0417df1ad44d4
0xR <114037840+0xRum@users.noreply.github.com>
2024-11-07 17:20:19 -0500

    todo/comments for attributes

--- Document.java
+++ Document.java
@@ -1,11 +1,27 @@
 import java.util.Date;
 
+//TODO
+//Things to do for the doc summarizer.
+//use a word count for the document reader?
+//these words will be used in the summarization process
+//use lowering text/tolowercase
+//remove stop words
+//use lemmatization or stemming to reduce words to their root form
+//use tf-idf to calculate word importance
+//use a summary length for the document reader
+//use a summarization algorithm to generate a summary of the document from the abstract or introduction
+
 public class Document {
     // Attributes
     private String fileName;
     private String fileType;
     private double fileSize;
     private Date uploadTime;
+    // name
+    // authors
+    // tags
+    // type
+    // keywords
 
     // Constructors
     public Document(String fileName, String fileType, double fileSize, Date uploadTime) {
