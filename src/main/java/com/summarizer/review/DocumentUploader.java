931d4196a8a8ae35823c6c20d144ea81510faaf9
0xR <114037840+0xRum@users.noreply.github.com>
2024-11-07 18:24:23 -0500

    renamed document class/moved
    renamed to DocumentUploader

--- src/main/java/com/summarizer/review/DocumentUploader.java
+++ src/main/java/com/summarizer/review/DocumentUploader.java
@@ -1,30 +1,21 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Date;
+import java.util.Arrays;
+import java.util.List;
 
-//TODO
-//Things to do for the doc summarizer.
-//use a word count for the document reader?
-//these words will be used in the summarization process
-//use lowering text/tolowercase
-//remove stop words
-//use lemmatization or stemming to reduce words to their root form
-//use tf-idf to calculate word importance
-//use a summary length for the document reader
-//use a summarization algorithm to generate a summary of the document from the abstract or introduction
-
-public class Document {
+public class DocumentUploader {
     // Attributes
+    private String filePath;
+    private String fileFormat;
+    private String fileContent;
     private String fileName;
     private String fileType;
     private double fileSize;
     private Date uploadTime;
-    // name
-    // authors
-    // tags
-    // type
-    // keywords
 
     // Constructors
-    public Document(String fileName, String fileType, double fileSize, Date uploadTime) {
+    public DocumentUploader(String fileName, String fileType, double fileSize, Date uploadTime) {
         this.fileName = fileName;
         this.fileType = fileType;
         this.fileSize = fileSize;
