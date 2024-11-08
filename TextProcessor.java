1543b53ff26239c44951cad03b363b0d40d42927
6iaka <siaka.diarra@maine.edu>
2024-11-08 03:10:18 -0500

    Initial commit with basic TextProcessor class

---
+++ TextProcessor.java
@@ -0,0 +1,19 @@
+public class TextProcessor {
+    // Attributes
+    private String document;
+
+    // Constructor
+    public TextProcessor(String document) {
+        this.document = document;
+    }
+
+    // Getter
+    public String getDocument() {
+        return document;
+    }
+
+    // Setter
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
