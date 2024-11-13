600562c8fa499f86b5b8f808fc7fe76b8d707374
Ms-Walsbee <kursten.massey@maine.edu>
2024-11-13 16:49:28 -0500

    Create DatabaseManager.java

---
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
@@ -0,0 +1,63 @@
+package LegalDocSummarizer.src.main.java.com.summarizer.review;
+
+import java.util.Date;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class DatabaseManager {
+    //Attributes
+    private String summaries;
+    private String userSummaries;
+    private Date lastUpdated;
+    private int documentID; 
+
+    //Constructors
+    public DatabaseManager(String summaries, String userSummaries, Date lastUpdated, int documentID){
+        this.summaries = summaries;
+        this.userSummaries = userSummaries;
+        this.lastUpdated = lastUpdated;
+        this.documentID = documentID;
+    }
+
+    //Getters
+    public String getSummaries(){
+        return summaries;
+    }
+
+    public String getUserSummaries(){
+        return userSummaries;
+    }
+
+    public Date getLastUpdated(){
+        return lastUpdated;
+    }
+
+    public int getDocumentID(){
+        return documentID;
+    }
+
+    //Setters
+    public void setSummaries(String summaries){
+
+    }
+
+    public void setUserSummaries(String usersummaries){
+
+    }
+
+    public void setLastUpdated(Date lastUpdated){
+
+    }
+
+    public void setDocumentID(int documentID){
+
+    }
+    
+    //Methods
+
+
+    //Connection String
+}
