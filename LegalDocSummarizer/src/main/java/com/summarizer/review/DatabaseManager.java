8e7b27c14911ae4c188435c1e97a524a4cd6b8e8
Ms-Walsbee <kursten.massey@maine.edu>
2024-11-13 16:29:33 -0500

    Added Attributes and Imports with skeleton for code

--- LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
@@ -1,5 +1,25 @@
 package LegalDocSummarizer.src.main.java.com.summarizer.review;
 
+import java.util.Date;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class DatabaseManager {
+    //Attributes
+    private String summaries;
+    private String userSummaries;
+    private Date lastUpdated;
+    private int documentID; 
+
+    //Setters
+
+
+    //Getters
+
     
+    //Methods
+
 }
