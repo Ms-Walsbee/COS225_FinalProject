50d7da3a51563ba6b4d42e37ecbbc69a1ad28c0a
Ms-Walsbee <kursten.massey@maine.edu>
2024-11-13 12:27:07 -0500

    Added calculateTFIDF method and helpers

--- Summarizer.java
+++ Summarizer.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Summarizer{
     //Attributes
@@ -53,6 +54,70 @@ public class Summarizer{
     }
 
     //Methods 
+    public void calculateTFIDF(List<String> document) {
+        Map<String, Integer> termFrequencey = new HashMap<>();
+        int totalWords = document.size();
+
+        //Calculate the Term Frequency (TF)
+        for(String word : document) {
+            word = word.toLowerCase();
+            termFrequencey.put(word, termFrequencey.getOrDefault(word, 0) + 1);
+        }
+
+        //Calculate TFIDF for each word
+        for(Map.Entry<String, Integer> entry : termFrequencey.entrySet()) {
+            String word = entry.getKey();
+            int count = entry.getValue();
+
+            double tf = (double) count / totalWords;
+
+            double idf = Math.log(1.0 +1.0);
+
+            double tfidf = tf * idf;
+
+            tdfifScores.put(word, tfidf);
+        }
+
+        //Ranks the sentences based on the highest scoring words
+        rankSentencesByTFIDF(document);
+    }
+
+    //Rank sentences based on the sum total of the TFIDF scores of the words in the sentence
+    private void rankSentencesByTFIDF(List<String> document) {
+        List<String> sentences = splitDocumentIntoSentences(document);
+        for(String sentence : sentences) {
+            double senteceScore = 0.0;
+            String[] words = sentence.split("\\s+");
+
+            for(String word : words) {
+                word = word.toLowerCase();
+                senteceScore += tdfifScores.getOrDefault(word, 0.0);
+            }
+
+            sentenceRankings.put(sentence, senteceScore);
+        }
+    }
+
+    //Helps the method split the document into sentences
+    private List<String> splitDocumentIntoSentences(List<String> document){
+        String documentText = String.join(" ", document);
+
+        String sentenceDelimiterRegex = "(?<!\\b(?:Mr|Ms|Dr|Jr|Sr|Inc|e\\.g|i\\.e|U\\.S|etc))([.!?])\\s+";
+        
+        Pattern pattern = Pattern.compile(sentenceDelimiterRegex);
+        String[] sentences = pattern.split(documentText);
+
+        List<String> sentenceList = new ArrayList<>();
+        for(String sentence : sentences) {
+            if(!sentence.trim().isEmpty()) {
+                sentenceList.add(sentence.trim());
+            }
+        }
+
+        return sentenceList;
+    }
+
+
     public String generateSummary(List<String> sentences) {
 
         //Sorts sentences based on their rankings of how important the words are in that sentence
