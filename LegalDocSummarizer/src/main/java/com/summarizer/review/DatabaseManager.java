4011c5ea45bb18259929f5e2aa48195cc9d20004
Ms-Walsbee <kursten.massey@maine.edu>
2024-11-14 11:16:25 -0500

    Create Document class
    After meeting with VJ we needed to take code out of DatabaseManager and put it in a new class

--- LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
@@ -1,7 +1,5 @@
 package com.summarizer.review;
 
-import java.util.Date;
-
 //add databasemanager attributes getters and setters to a document java class.
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -9,46 +7,6 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class DatabaseManager {
-    // Attributes
-    private String summaries;
-    private String userSummaries;
-    private Date lastUpdated;
-    private int documentID;
-
-    // Getters
-    public String getSummaries() {
-        return summaries;
-    }
-
-    public String getUserSummaries() {
-        return userSummaries;
-    }
-
-    public Date getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public int getDocumentID() {
-        return documentID;
-    }
-
-    // Setters
-    public void setSummaries(String summaries) {
-
-    }
-
-    public void setUserSummaries(String usersummaries) {
-
-    }
-
-    public void setLastUpdated(Date lastUpdated) {
-
-    }
-
-    public void setDocumentID(int documentID) {
-
-    }
-
     // Methods
 
     // Connection String
---
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/Document.java
@@ -0,0 +1,47 @@
+package com.summarizer.review;
+
+import java.util.Date;
+
+public class Document {
+      // Attributes
+    private String summaries;
+    private String userSummaries;
+    private Date lastUpdated;
+    private int documentID;
+
+    // Getters
+    public String getSummaries() {
+        return summaries;
+    }
+
+    public String getUserSummaries() {
+        return userSummaries;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public int getDocumentID() {
+        return documentID;
+    }
+
+    // Setters
+    public void setSummaries(String summaries) {
+
+    }
+
+    public void setUserSummaries(String usersummaries) {
+
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+
+    }
+
+    public void setDocumentID(int documentID) {
+
+    }
+
+    //Methods
+}
