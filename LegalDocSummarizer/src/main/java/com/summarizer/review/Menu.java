743627f10f80160696d1f343a56784efc73de93f
0xR <114037840+0xRum@users.noreply.github.com>
2024-11-13 20:26:13 -0500

    added methods and options
    added options buttons, empty startup and shutdown methods

--- LegalDocSummarizer/src/main/java/com/summarizer/review/Menu.java
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/Menu.java
@@ -1,7 +1,27 @@
 package com.summarizer.review;
 
 public class Menu {
+
+    public void startUp() {
+
+    }
+
+    public void shutDown() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Legal Doc Summ...");
+        System.out.println("Starting the Legal Doc Summarizer..");
+
+        System.out.println("Hello! Welcome to the Summarizer!");
+
+        System.out.println("Please select one of the following options: ");
+
+        System.out.println("1. Upload legal document.");
+        System.out.println("2. Generate summary of a document.");
+        System.out.println("3. Retrieve past summary from the database.");
+        System.out.println("4. Exit.");
+
+        System.out.print("Enter you choice: ");
     }
 }
