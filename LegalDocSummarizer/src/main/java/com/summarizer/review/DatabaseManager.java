e14acd9ad314e98877edcd491c7e910be8a1e767
0xR <114037840+0xRum@users.noreply.github.com>
2024-12-13 00:44:29 -0500

    added javadocs
    Javadocs to DatabaseManager, DocumentUploader, and Menu classes

--- LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/DatabaseManager.java
@@ -14,120 +14,188 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+/**
+ * This DatabaseManager class the MongoDB database connection and provides
+ * methods for adding documents,
+ * deleting documents, and deleting the entire collection.
+ * 
+ */
+
 public class DatabaseManager {
     private String connectionString, databaseName, collectionName;
 
-    // Constructor with default connection string
+    /**
+     * This constructs a new DatabaseManager with the provided database and
+     * collection names.
+     * 
+     * @param dbName         Name of the database to connect to.
+     * @param collectionName Name of the collection to with the database.
+     */
     public DatabaseManager(String dbName, String collectionName) {
+
         try (BufferedReader reader = new BufferedReader(new FileReader(
                 "src/main/resources/connection.txt"))) {
+
             connectionString = reader.readLine();
         } catch (IOException e) {
+
             System.out.println("Error reading connection string from 'connection.txt'");
             e.printStackTrace();
         }
+
         this.databaseName = dbName;
+
         this.collectionName = collectionName;
     }
 
     // Add a Document to the database collection
+    /**
+     * Adds a single Document to the database "doc_data" collection.
+     * 
+     * @param document The document to be added to the database.
+     * @return Returns result of adding the document, null if error.
+     */
     public InsertOneResult addToDatabase(Document document) {
+
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
+
             MongoCollection<Document> collection = database.getCollection(collectionName);
 
             return collection.insertOne(document);
 
-            // System.out.println("Document added to the database successfully!");
         } catch (Exception e) {
+
             System.out.println("An error occurred while adding the document to the database: " + e.getMessage());
+
             return null;
         }
     }
 
-    // Create a collection in the database
+    /**
+     * Create a collection in the database with the collection name.
+     */
     public void createCollection() {
+
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
+
             database.createCollection(collectionName);
+
             System.out.print("\033[0;36mCollection created successfully!\033[0m");
         } catch (Exception e) {
+
             System.out.println("An error occurred while creating the collection: " + e.getMessage());
         }
     }
 
-    // Delete the collection from the database
+    /**
+     * Deletes the collection in the database.
+     */
     public void deleteCollection() {
+
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
+
             database.getCollection(collectionName).drop();
-            // System.out.println("Collection deleted successfully!");
+
         }
     }
 
-    // Delete all documents in the collection
+    /**
+     * Deletes all documents in the collections.
+     */
     public void deleteAllDocuments() {
+
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
+
             MongoCollection<Document> collection = database.getCollection(collectionName);
+
             collection.deleteMany(new Document());
+
             System.out.println("All documents deleted successfully!");
         }
     }
 
+    /**
+     * This retrieves documents from the database where the "authors" field matches
+     * the the inputed author string.
+     * 
+     * @param author The name or part of the authors name to search for.
+     * @return The list of documents matching the "authors" query.
+     */
     public List<Document> getDocumentsByAuthor(String author) {
+
         List<Document> documents = new ArrayList<>();
+
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
+
             MongoCollection<Document> collection = database.getCollection(collectionName);
+
             Document query = new Document("authors", new Document("$regex", author).append("$options", "i"));
+
             for (Document doc : collection.find(query)) {
                 documents.add(doc);
             }
 
         } catch (Exception e) {
-            // Print an error message if an exception occurs
             System.out.println("An error occurred while retrieving the document: " + e.getMessage());
         }
         return documents;
 
     }
 
+    /**
+     * This retrieves documents from the database where the "title" field matches
+     * the the inputed title string.
+     * 
+     * @param title The title or part of the title search for.
+     * @return A list of documents matching the title query.
+     */
     public List<Document> getDocumentsByTitle(String title) {
 
         List<Document> documents = new ArrayList<>();
 
-        // Use a try-with-resources statement to ensure the MongoClient is closed
-        // automatically
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
-            // Connect to the specified database
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
 
-            // Access the specified collection within the database
             MongoCollection<Document> collection = database.getCollection(collectionName);
 
-            // Create a query to find a document
             Document query = new Document("title", new Document("$regex", title).append("$options", "i"));
-            // Document doc = collection.find(query).first();
+
             for (Document document : collection.find(query)) {
                 documents.add(document);
             }
         } catch (Exception e) {
-            // Print an error message if an exception occurs
             System.out.println("An error occurred while retrieving the document: " + e.getMessage());
-
-            // Return null if the document could not be retrieved
-            // return null;
+            return null;
         }
         return documents;
     }
 
-    //Fetch all the documents frome the collection
+    /**
+     * This method retrieves all documents in the "doc_data" collection to display
+     * summaries.
+     * 
+     * @return documents
+     */
     public List<Document> getAllDocuments() {
         List<Document> documents = new ArrayList<>();
+
         try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+
             MongoDatabase database = mongoClient.getDatabase(databaseName);
+
             MongoCollection<Document> collection = database.getCollection(collectionName);
+
             for (Document doc : collection.find()) {
                 documents.add(doc);
             }
--- LegalDocSummarizer/src/main/java/com/summarizer/review/DocumentUploader.java
+++ LegalDocSummarizer/src/main/java/com/summarizer/review/DocumentUploader.java
@@ -1,16 +1,28 @@
-//Document uploader
 package com.summarizer.review;
 
 import org.bson.Document;
 
+/**
+ * DocumentUploader is responsible for creating and managing document objects
+ * for upload to MongoDB. Takes metadata for title, authors, overview, and
+ * categories.
+ */
 public class DocumentUploader {
-    // Attributes
     private String title;
     private String authors;
     private String overview;
     private String categories;
 
-    // Constructors/initialization for file details and checking formats
+    /**
+     * Constructors/initialization for the documents metadata which will be used to
+     * create the MongoDB document.
+     * 
+     * @param title      The title of the document.
+     * @param authors    The authors of the document.
+     * @param overview   The overview of the document.
+     * @param categories The categories of the document.
+     */
+
     public DocumentUploader(String title, String authors, String overview, String categories) {
         this.title = title;
         this.authors = authors;
@@ -18,6 +30,11 @@ public class DocumentUploader {
         this.categories = categories;
     }
 
+    /**
+     * This creates and returns a MongoDB document object from the current database.
+     * 
+     * @return
+     */
     public Document getDocument() {
         Document document = new Document();
         document.append("title", title);
